19ea0d58b415f86b121587065e3561aa7f389d0d
anh.thi <thi12171999@gmail.com>
2021-08-18 19:04:40 +0700

    Khởi tạo Project

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+const PORT = process.env.PORT || 3000
+app.use(cors())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,POST')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    next()
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running at ${PORT}`)
+})
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`)
+})
+
+module.exports = app
